6ae2882646bc285f7f75ce35eb7f505e9cd868eb
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-23 18:27:22 +0100

    Initial commit

---
+++ src/VideoConsumingService.java
@@ -0,0 +1,24 @@
+public class VideoConsumingService {
+
+
+}
+
+
+class Video{
+
+    String id;
+    Frame[] frames;
+    String jsonMetaData;
+
+}
+
+class Frame{
+    btye[] btyes;
+    int timestamp;
+}
+
+class User{
+    String id;
+    String name;
+    String email;
+}
