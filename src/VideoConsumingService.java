e1e2cf2f949283a4a29ef01087330abdfc7beb39
Chanel Morgan <chanelmorgan05@icloud.com>
2024-04-23 18:41:30 +0100

    Creating the basic outline of classes from a UML to pratice a low level system design

--- src/VideoConsumingService.java
+++ src/VideoConsumingService.java
@@ -1,8 +1,40 @@
 public class VideoConsumingService {
 
+    private Database database;
+
+    public int seekTime(String userId, String videoId){
+        WatchedVideo watchedVideo = database.getWatchedVideo(userId, videoId);
+        return watchedVideo.getSeekTime();
+
+    }
 
 }
 
+
+class VideoService{
+    private FileSystem fileSystem;
+
+    public Frame getFrame(String videoId, int timeStamp){
+        Video video = fileSystem.getVideo(videoId);
+        return video.getFrame(timeStamp);
+    }
+}
+
+class FileSystem{
+
+    public Video getVideo(String videoId){
+        return null;
+    }
+
+}
+
+class Database{
+    public WatchedVideo getWatchedVideo(String userId, String vidoeId){
+        return null;
+
+    }
+
+}
 
 class Video{
 
@@ -10,15 +42,47 @@ class Video{
     Frame[] frames;
     String jsonMetaData;
 
+    public Frame getFrame(int timestamp){
+        for(int i =0; i < frames.length; i++) {
+
+            if(frames[i].startTimestamp <= timestamp
+                    && frames[i].endTimestamp > timestamp) {
+                return frames[i];
+            }
+        }
+        throw new IndexOutOfBoundsException();
+
+    }
+
 }
 
 class Frame{
-    btye[] btyes;
-    int timestamp;
+
+    public static int frameTime = 10;
+    byte[] bytes;
+    int startTimestamp;
+    int endTimestamp;
+
+
 }
 
 class User{
     String id;
     String name;
     String email;
+
+    public String getId(){
+        return id;
+    }
+}
+
+class WatchedVideo{
+    String videoId;
+    String userId;
+    int seekTime;
+
+
+    public int getSeekTime(){
+        return seekTime;
+    }
 }
